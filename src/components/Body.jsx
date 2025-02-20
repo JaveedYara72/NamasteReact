dd987343fe354dfaa44d5ebf5583da026e7895cc
JaveedYara72 <javeedymd.iiitg@gmail.com>
2025-02-20 14:10:34 +0530

    added useEffect, and styles  for button

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,16 +3,35 @@ import RestaurantCard from "./RestaurantCard";
 
 // Constant important
 import { swiggyData } from "../../src/utils/mockData";
+import { useState, useEffect } from "react";
 
 // Key for the props
 let restaurantCounter = 0;
 
 const Body = () => {
+  // Writing useState hook
+  const [listOfRestaurants, setListOfRestaurants] = useState(
+    swiggyData?.data?.cards
+  );
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log(listOfRestaurants);
+            const filteredList = listOfRestaurants.filter(
+              (restaurant) => restaurant.card.card.info.avgRating < 4.0
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Filter Restaurants
+        </button>
+      </div>
       <div className="restaurant-container">
-        {swiggyData?.data?.cards?.map((restaurant) => {
+        {listOfRestaurants.map((restaurant) => {
           restaurantCounter++;
           return (
             <RestaurantCard
